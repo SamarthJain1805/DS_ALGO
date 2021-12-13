15a5887924bb1907c824e25dd3a97f6f5bc72998
Samarth Jain <samarthjain1805@gmail.com>
2021-12-13 22:29:56 +0530

    equilibrium point in an array.

--- DSA ONE/src/arrays/EquilibriumPoint.java
+++ DSA ONE/src/arrays/EquilibriumPoint.java
@@ -20,15 +20,15 @@ public class EquilibriumPoint {
 	static boolean isEquiPoint(int arr[]) {
 		int sum =0;
 		int n = arr.length;
-		for(int i=0;i<n;i++) 
+		for(int i=0;i<n;i++) {
 			sum+=arr[i];
-			
+		}
 		int l_sum=0;
-		for(int j=0;j<n;j++) {
-			if(l_sum == sum-arr[j])
+		for(int i=0;i<n;i++) {
+			if(l_sum == sum-arr[i])
 				return true;
-		l_sum+=arr[j];
-		sum-=arr[j];
+		l_sum+=arr[i];
+		sum-=arr[i];
 		}
 		return false;
 	}
@@ -36,7 +36,7 @@ public class EquilibriumPoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int arr[] = {3,4,8,-9,20,5};
+		int arr[] = {3,4,8,-9,20,6};
 		
 		System.out.println(isEquiPoint(arr));
 		
